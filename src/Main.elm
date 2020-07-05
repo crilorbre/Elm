85f9d478bb444d5f9b24eee5e16b51abd72bd987
crilorbre <43986572+crilorbre@users.noreply.github.com>
2020-07-05 18:56:07 +0200

    modificaciones en el estilo

--- src/Main.elm
+++ src/Main.elm
@@ -21,7 +21,8 @@ type alias Model =
   { 
     estatura: String,
     peso: String,
-    resultado: String
+    resultado: String,
+    interpretacion: String
   }
 
 init : Model
@@ -29,6 +30,7 @@ init =
   {estatura = ""
   , peso = ""
   , resultado = ""
+  , interpretacion = ""
   } 
 
 
@@ -40,6 +42,8 @@ type Msg
   = Estatura String
   | Peso String
   | Resultado
+  | Interpretacion
+  | Clear
 
 
 update : Msg -> Model -> Model
@@ -51,8 +55,21 @@ update msg model =
     Peso nuevoPeso ->
       {model | peso  =  nuevoPeso}
 
-    Resultado->
-      {model | resultado = obtenerResultado model.estatura model.peso}
+    Resultado ->
+     {model | resultado = obtenerResultado model.estatura model.peso }
+
+    Interpretacion ->
+      if Maybe.withDefault 0.0 (String.toFloat (obtenerIMC model.estatura model.peso)) < 18.5 then
+        update Resultado {model|interpretacion = "Bajo peso"}
+      else if Maybe.withDefault 0.0 (String.toFloat (obtenerIMC model.estatura model.peso)) >= 18.5 && Maybe.withDefault 0.0 (String.toFloat (obtenerIMC model.estatura model.peso)) < 25 then
+        update Resultado {model|interpretacion = "Normal"}
+      else if Maybe.withDefault 0.0 (String.toFloat (obtenerIMC model.estatura model.peso)) >= 25 && Maybe.withDefault 0.0 (String.toFloat (obtenerIMC model.estatura model.peso)) < 30 then
+        update Resultado {model|interpretacion = "Sobrepeso"}
+      else 
+        update Resultado {model|interpretacion = "Obesidad"}
+
+    Clear ->
+      init
 
 
 
@@ -78,29 +95,39 @@ obtenerIMC estatura peso =
 
 view : Model -> Html Msg
 view model =
-  div[style "display" "flex", style "flex-direction" "column", style "justify-content" "center", style "align-items" "center", style "min-height" "80vh"] 
+  div[style "display" "flex", style "flex-direction" "column", style "justify-content" "center", style "align-items" "center", style "min-height" "100vh"]  
   [ 
-    div [] [
-      h1 [] [text "Calculadora del índice de masa corportal (IMC)"]
-    ]
-    ,
+    div [style "background" "linear-gradient(to right, #44ddd0, #27d7a1)", style "border-radius" "5px", style "padding" "60px 40px", style "position" "relative"][
 
-    div[] [
-    label [] [text "Introduzca su estatura (en metros): "]
-    , input [type_ "number",  placeholder "Introduce tu estatura", value  model.estatura, onInput Estatura] []
+      div [] [
+        h1 [style "text-align" "center"] [text "Calculadora del índice de masa corportal (IMC)"]
+      ]
+      ,
+      div [][
+        
+        div[] [
+        label [style "font-size" "20px"] [text "Introduzca su estatura (en metros): "]
+        , input [style "width" "100%", style "margin" "15px 0", style "padding" "12px 0px", 
+        type_ "number",  placeholder "Introduce tu estatura...", value  model.estatura, onInput Estatura] []
+        ]
+        , 
+        
+        div[] [
+          label [style "font-size" "20px"] [text "Introduzca su peso (en kgs): "]
+          ,input [style "width" "100%", style "margin" "15px 0", style "padding" "12px 0px", 
+          type_ "number",  placeholder "Introduce tu peso...", value  model.peso, onInput Peso] []
+        ]
+      ]
+      
+      ,
+
+      div[] [
+      text  model.resultado
+      ]
+      ,
+      viewValidation model,
+      viewInterpretacion model
     ]
-    , 
-    
-    div[] [
-      label [] [text "Introduzca su peso (en kgs): "]
-      ,input [ type_ "number",  placeholder "Introduce tu peso", value  model.peso, onInput Peso] []
-    ]
-  
-    , div[] [
-    text  model.resultado
-    ]
-    ,
-    viewValidation model
   ]
 
 
@@ -109,13 +136,31 @@ viewValidation : Model -> Html Msg
 
 viewValidation model =
   if model.estatura > "0"  && model.peso > "0" then
-    button [ onClick Resultado ] [ text "Calcule su IMC" ]
+    div [style "position" "absolute", style "right" "25px", style "bottom" "25px", style "border-radius" "8px"] [
+      button [ style "padding" "12px 30px", onClick Interpretacion ] [ text "Calcule su IMC" ]
+      , 
+      button [ style "padding" "12px 30px", onClick Clear ] [ text "Clear" ]
+    ]
+    
   else if model.estatura /= "" && model.estatura <= "0" then
     div [ style "color" "red" ] [ text "Por favor, introduce una estatura válida" ]
   else if model.peso /= "" && model.peso <= "0" then
     div [ style "color" "red" ] [ text "Por favor, introduce un peso válido" ]
   else
     div [][]
+
+viewInterpretacion: Model -> Html Msg
+viewInterpretacion model =
+  if model.interpretacion /= "" then
+    if model.interpretacion == "Bajo peso" then
+      div [ style "color" "red" ] [ text "Bajo peso" ]
+    else if model.interpretacion == "Normal" then
+      div [ style "color" "red" ] [ text "Normal" ]
+    else if model.interpretacion == "Sobrepeso" then
+      div [ style "color" "red" ] [ text "Sobrepeso" ]
+    else 
+      div [ style "color" "red" ] [ text "Obesidad" ]
+  else
+    div [][]
+
     
-
-
